6f9ddb9f2d24466939b87c3df5bac69ea9f667c5
Carlos Jimeno Cordero <jimenocorderocarlos5@gmail.com>
2017-10-20 11:36:23 +0200

    Actualizado

--- Practica 2/practica2.py
+++ Practica 2/practica2.py
@@ -1,10 +1,25 @@
 # encoding: latin1
 
+import urllib2, re
+from Tkinter import *
+import tkMessageBox
+import sqlite3
+from bs4 import BeautifulSoup
+
+
 # Enlace a la práctica:
 # http://www.lsi.us.es/docencia/get.php?id=8986
 
 # A - Almacenar Categorías
+def extraer_datos():
+    file = urllib2.urlopen("http://www.sevillaguia.com/sevillaguia/agendacultural/agendacultural.asp")
+    soup = BeautifulSoup(file, 'html.parser')
+    print soup.find_all().__doc__
+
 
 # B - Buscar Categorías
 
 # C - Buscar Evento
+
+if __name__ == '__main__':
+    extraer_datos()
