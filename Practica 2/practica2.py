77e8c38811a58d9df7d44fe9c3577a30011ff2fa
Carlos Jimeno Cordero <jimenocorderocarlos5@gmail.com>
2017-10-20 12:11:09 +0200

    Actualizado

--- Practica 2/practica2.py
+++ Practica 2/practica2.py
@@ -14,7 +14,24 @@ from bs4 import BeautifulSoup
 def extraer_datos():
     file = urllib2.urlopen("http://www.sevillaguia.com/sevillaguia/agendacultural/agendacultural.asp")
     soup = BeautifulSoup(file, 'html.parser')
-    print soup.find_all().__doc__
+    columna_noticias = soup.find('td', width='135', valign='top')
+    datos = []
+    categoria_actual = 'None'
+    for tr in columna_noticias.find_all('tr'):
+        if tr.find(class_='TituloIndice'):
+            # ha encontrado una categoría
+            categoria = tr.find(class_='TituloIndice').text.strip()
+            categoria_actual = categoria
+            print categoria
+        elif tr.find(class_='LinkIndice'):
+            d = tr.find(class_='LinkIndice')
+            # título link categoria
+            titulo = d.text.strip()
+            link = d['href'].strip()
+            datos.append((titulo, link, categoria_actual))
+        else:
+            print 'otra cosa'
+    return datos
 
 
 # B - Buscar Categorías
