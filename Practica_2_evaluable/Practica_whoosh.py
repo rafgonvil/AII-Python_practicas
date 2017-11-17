04946b9c83d551e198df2039c54b89367498567c
Carlos Jimeno Cordero <jimenocorderocarlos5@gmail.com>
2017-11-17 10:21:26 +0100

    Actualizado

--- Practica_2_evaluable/Practica_whoosh.py
+++ Practica_2_evaluable/Practica_whoosh.py
@@ -4,7 +4,32 @@ from __future__ import print_function
 from Tkinter import *
 import tkMessageBox
 
-    
+def extraer_datos():
+    """
+    Extraer todos los datos utilizando BS4 y devolverlos
+    """
+
+def cargar_command():
+    """
+    1 - Extraer datos de la web con BS4
+    2 - Guardar los datos con Whoosh
+    3 - Notificar con una ventana emergente
+    """
+    pass
+
+
+def buscar_1():
+    pass
+
+
+def buscar_2():
+    pass
+
+
+def buscar_3():
+    pass
+
+
 def ventana_principal():
     top = Tk()
     menubar_top = Menu(top)
@@ -12,26 +37,23 @@ def ventana_principal():
     # Datos
     datos_menu = Menu(menubar_top, tearoff=0)
     menubar_top.add_cascade(label="Datos", menu=datos_menu)
-    datos_menu.add_command(label="Cargar")#, command=indexar_command)
+    datos_menu.add_command(label="Cargar", command=cargar_command)
     datos_menu.add_separator()
     datos_menu.add_command(label="Salir", command=top.quit)
-    
 
     # Buscar
     buscar_menu = Menu(menubar_top, tearoff=0)
     menubar_top.add_cascade(label="Buscar", menu=buscar_menu)
 
-    # Buscar/Noticia
-    buscar_menu.add_command(label="Noticia")
-    buscar_menu.add_command(label="Fecha")#, command=buscar_titulo_command)
-    buscar_menu.add_command(label="Autor")#, command=buscar_autor_command)
+    # Buscar/Comandos
+    buscar_menu.add_command(label="Noticia", command=buscar_1)
+    buscar_menu.add_command(label="Fecha", command=buscar_2)
+    buscar_menu.add_command(label="Autor", command=buscar_3)
 
-    #Buscar/ Autor
+    # Buscar/ Autor
     top.config(menu=menubar_top)
     top.mainloop()
 
-def do_nothing():
-     pass
 
 if __name__ == '__main__':
     ventana_principal()
