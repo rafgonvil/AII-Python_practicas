e645f8c96f52c2fc901b2e469c18173abd282f3c
Carlos Jimeno Cordero <jimenocorderocarlos5@gmail.com>
2017-12-01 11:03:06 +0100

    wasd

--- DjangoP1G2/principal/models.py
+++ DjangoP1G2/principal/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from pattern.db import primary_key
 
 # Create your models here.
+class Diario(models.Model):
+    nombre = models.CharField(max_length = 30)
+    pais = models.CharField(max_length = 30)
+    idioma = models.CharField(max_length = 30)
+    
+class Usuario(models.Model):
+    nombre_usuario = models.CharField(max_length = 30)
+    passwd = models.CharField(max_length = 30)
+    email = models.EmailField()
+    nombre = models.CharField(max_length = 30)
+    apellidos = models.CharField(max_length = 30)
+    
+class Autor(models.Model):
+    nombre_usuario = models.CharField(max_length = 30)
+    passwd = models.CharField(max_length = 30)
+    email = models.EmailField()
+    nombre = models.CharField(max_length = 30)
+    apellidos = models.CharField(max_length = 30)
+
+class Tipo_noticia(models.Model):
+    tiponotId = models.CharField(max_length = 20,primary_key = True)
+    descripcion = models.TextField()
+
+class Noticia(models.Model):
+    usuarios = models.ManyToManyField(Usuario,verbose_name = "usuarios interesados")
+    fecha = models.DateField()
+    diario = models.OneToOneField(Diario,verbose_name = "diario")
+    titular = models.CharField(max_length = 50)
+    autores = models.ManyToManyField(Autor,verbose_name = "autores")
+    resumen = models.TextField()
+    tipo = models.OneToOneField(Tipo_noticia,verbose_name = "tipo noticia")
+    
+
+    
